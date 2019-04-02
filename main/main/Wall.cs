40f2ec31166c6001390ca639de668988b437c140
Tymoteusz Skorka <skorkatymoteusz@gmail.com>
2019-04-02 23:36:17 +0200

    Some infrastructure of board

---
+++ main/main/Wall.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace main
+{
+    class Wall : Field
+    {
+        public Wall(short a_16StartPositionX, short a_16StartPositionY, bool a_ifVertical, bool a_ifUp)
+        {
+
+        }
+
+        public override void Move()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
