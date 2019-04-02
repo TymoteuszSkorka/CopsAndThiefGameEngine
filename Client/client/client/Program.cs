518bda43d9b996b2d92d5fb53e983c5cd58591c2
MarcelVi <marcel.wilanowski97@gmail.com>
2019-04-02 13:50:27 +0200

    client-server

---
+++ Client/client/client/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Net;
+using System.Threading;
+
+namespace client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int port = 13000;
+            string IpAddress = "127.0.0.1";
+            Socket ClientSocket = new Socket(AddressFamily
+                .InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPEndPoint ep = new IPEndPoint(IPAddress.Parse(IpAddress), port);
+            ClientSocket.Connect(ep);
+            Console.WriteLine("Client is connected!");
+            while (true)
+            {
+                string messageFromClient = null;
+                Console.WriteLine("Ented the message");
+                messageFromClient = Console.ReadLine();
+                ClientSocket.Send(System.Text.Encoding.ASCII.GetBytes(messageFromClient),
+                    0, messageFromClient.Length, SocketFlags.None);
+
+                byte[] MsgFromServer = new byte[1024];
+                int size = ClientSocket.Receive(MsgFromServer);
+                Console.WriteLine("Server answer: " + System.Text.Encoding.ASCII.GetString(MsgFromServer, 0, size));
+
+            }
+        }
+    }
+}
---
+++ Server/Server/Server/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Net;
+using System.Threading;
+namespace Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int port = 13000;
+            string IpAddress = "127.0.0.1";
+            Socket ServerListener = new Socket(AddressFamily
+                .InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPEndPoint ep = new IPEndPoint(IPAddress.Parse(IpAddress), port);
+            ServerListener.Bind(ep);
+            ServerListener.Listen(100);
+            Console.WriteLine("Server is Listening ...");
+            Socket ClientSocket = default(Socket);
+
+            int counter = 0;
+            Program p = new Program();
+            while (true)
+            {
+                counter++;
+                ClientSocket = ServerListener.Accept();
+                Console.WriteLine(counter + " Clients connected");
+                Thread UserThread = new Thread(new ThreadStart(()=>p.User(ClientSocket)));
+                UserThread.Start();
+            }
+        
+        }
+        public void User(Socket client)
+        {
+            while (true)
+            {
+                byte[] msg = new byte[1024];
+                int size = client.Receive(msg);
+                string lol = "lol";
+
+                string asciiString = Encoding.ASCII.GetString(msg, 0, msg.Length);
+                Console.WriteLine(asciiString);
+
+                if (msg.Length > 3)
+                {
+
+                    Console.WriteLine("weszlo");
+                    Map mapka = new Map();
+                    string s = mapka.Showmap();
+
+                    byte[] msg1 = Encoding.ASCII.GetBytes(s);
+                    int size2 = msg1.Length;
+
+                    client.Send(msg1, 0, size2, SocketFlags.None);
+                }
+                else
+                {
+                    client.Send(msg, 0, size, SocketFlags.None);
+                }
+            }
+        }
+    }
+}
